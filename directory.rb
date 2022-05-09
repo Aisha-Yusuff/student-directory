528d8154afd8627cce022be7257057e4a4e8d58c
Aisha Yusuff <a.yusuff99@outlook.com>
2022-05-09 15:49:32 +0000

    Add user input and input_students method

--- directory.rb
+++ directory.rb
@@ -13,6 +13,31 @@ students = [
  {name: "Norman Bates", cohort: :november}
  ]
  
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  
+  #create an empty array
+  students = []
+  
+  #get the first name
+  name = gets.chomp
+  
+  #while the mame is not empty, repeat the code above
+  while !name.empty? do 
+    
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    
+    #get another name from the user
+    name = gets.chomp
+  end 
+  
+  #return the array of students
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "--------------"
@@ -28,6 +53,8 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
+#nothing happens until we call the methods 
 print_header
 print(students)
 print_footer(students)
