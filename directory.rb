8006a7cc7d49c25c7c36643a0f3bb604bd030c65
Aisha Yusuff <a.yusuff99@outlook.com>
2022-05-10 17:00:02 +0000

    Add save_student method to save data

--- directory.rb
+++ directory.rb
@@ -1,7 +1,17 @@
 #Let's put all the students into an array
-
- 
 @students = [] # an empty array accessible to all methods
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end 
+  file.close
+end
 
 def input_students
   puts "Please enter the names of the students"
@@ -35,6 +45,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -50,6 +61,8 @@ def process(selection)
       input_students
     when "2"
      show_students
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to end
     else
@@ -71,5 +84,6 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
+
 
 interactive_menu
